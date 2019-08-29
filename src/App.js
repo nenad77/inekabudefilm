32757e2584fb898b39bbf2513a7c1759b91bce66
nenad77 <josic.nenad@gmail.com>
2019-08-29 19:19:37 +0200

    Nemanja

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 
-function App() {
-  return (
-    <Header>
-    
-    </Header>
-  );
+class App extends Component {
+  state = {
+      data: [],
+    };
+  
+  componentDidMount() {
+    fetch('http://www.omdbapi.com/?s=iron+man&apikey=3bb92b26')
+      .then(response => response.json())
+      .then(data => this.setState({ data: data.Search }));
+
+  }
+ 
+
+
+ render (){
+   return (
+     <React.Fragment>
+<Header/>
+   <ul>
+     {this.state.data.map(item => {
+      return <li key={item.imdbID}>{item.Title}<button onClick={this.addMovie}>+</button></li>
+     })}
+   </ul>
+   </React.Fragment>
+   )
+
+ }
 }
+// function App() {
+//   return (
+//     <Header>
+    
+//     </Header>
+//   );
+// }
 
 export default App;
+
+
+
